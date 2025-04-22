6e7e01ec95afab44a844f8addd288cbf488dd21f
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-04-22 10:38:07 +0530

    Functions.py

---
+++ Basics/Functions.py
@@ -0,0 +1,18 @@
+def go(name):
+    print(f"Hello, {name}!")
+
+go("Ashutosh")
+go("Omkar")
+
+Hello, Ashutosh!
+Hello, Omkar!
+
+---------------
+
+def go(a,b):
+    res=a+b
+    print(f"sum of numb is: {res}")
+    
+go(2,3)
+
+sum of numb is: 5
