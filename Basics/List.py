7b5b899a0572b71325719b00af7fbed533a86536
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-04-22 10:28:07 +0530

    List.py

--- Basics/List.py
+++ Basics/List.py
@@ -1,5 +1,34 @@
+🧺 What is a list?
+A list is a collection of items — like a box of tools.
 List – Ordered, changeable, allows duplicates
+
+Add items:
+tools.append("Spark")
+
+Remove items:
+tools.remove("SQL")
+
+
+tools = ["Python", "SQL", "Airflow"]
+print(tools)
+
+-------------
 
 fruits = ["apple", "banana", "cherry"]
 fruits.append("orange")  # add to list
 print(fruits[0])         # first item
+
+----------------
+
+skills = ["Python", "SQL"]
+
+skills.append("Airflow")
+
+for skill in skills:
+    print(skill)
+OUT-    
+Python
+SQL
+Airflow
+
+-------------------
