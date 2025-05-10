520901cb42aabf9360ebe6518e7b4e4a38f4bd49
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-05-10 15:33:38 +0530

    ifElse.py

--- Basics/startPy.py/BasicCodes/ifElse.py
+++ Basics/startPy.py/BasicCodes/ifElse.py
@@ -28,3 +28,34 @@ Enter value : 11
 2025-05-10 15:28:35.595 | INFO     | __main__:<module>:16 - Your value is less
 2025-05-10 15:28:35.595 | INFO     | __main__:<module>:17 - second line
 (base) ashutoshsanjayshimpi@192 PYTHON % 
+
+
+
+
+
+from loguru import logger
+import logging
+import math
+logging.basicConfig(
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+     datefmt="%Y-%m-%d %H:%M",
+     level=logging.DEBUG
+)
+
+
+
+area = int(input("Enter value : "))
+
+if(area<100):
+    logger.info(f"Your area is less")
+    logger.info("second line")
+
+elif area >100:
+    logger.info(f"You can build 2 buld")
+else:
+    logger.info(f"Share more details")
+    logger.info("third line")
+
+
+
