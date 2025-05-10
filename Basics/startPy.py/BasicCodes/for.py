9b3145777fe41fdc18b5ddd505a1390eff1a7810
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-05-10 17:39:39 +0530

    for.py

--- Basics/startPy.py/BasicCodes/for.py
+++ Basics/startPy.py/BasicCodes/for.py
@@ -18,3 +18,25 @@ for i in numbers:
 print("The sum of  is: ", a,i)     --62     and i gives total no. index 8
 
 
+
+
+
+
+from loguru import logger
+import logging
+import math
+logging.basicConfig(
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+     datefmt="%Y-%m-%d %H:%M",
+     level=logging.DEBUG
+)
+
+
+
+a = [3, 5, 6, 8, 4]  
+for i in range( len( a ) ):  
+    a.append(a[i] + 2)  
+print( a ) 
+
+[3, 5, 6, 8, 4, 5, 7, 8, 10, 6]    #same list + list+2  list append
