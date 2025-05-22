669020779931bfc0bb5b8debc50d575529475679
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-05-22 22:51:12 +0530

    Basics.py

---
+++ PySpark/Basics.py
@@ -0,0 +1,30 @@
+Type	         Example	         Ordered?	  Mutable?	 Key-Value?    Duplicates
+
+List	         [1, 2, 3]	          ✅	        ✅	        ❌        ✅	      
+Dictionary	 {'a': 1, 'b': 2}	      ✅	        ✅	        ✅        ❌ (keys)
+Set	          {1, 2, 3}	            ❌	        ✅	        ❌        ❌
+Tuple        	(1, 2, 3)	            ✅	        ❌	        ❌        ✅
+
+------------------------------------------------------------------------------------
+
+Type	                  Example             	              Has :?	  Ordered?	  Good for DataFrame?
+List of Dicts	 [{'name': 'Ashu', 'age': 24},{...}]	          ✅ Yes	   ✅ Yes	       ✅ Yes
+List of Sets  	[{'Ashu', 24}, {'keshu', 19}]	                ❌ No      ❌ No	         ❌ No
+
+
+
+
+
+#list of tuples 
+data = [("Ashu",24),("keshu",19)]
+col = ["Name","age"]
+df = spark.createDataFrame(data,col)
+df.show()
+
+
+#list of dictionaries.
+data = [{'id':1,'name':"Ashu"},{'id':2,'name':"keshu"}]
+df = spark.createDataFrame(data)
+df.show()
+
+#we cannot use list of sets with dataFrame. its only take value not key.  (error occurs)
