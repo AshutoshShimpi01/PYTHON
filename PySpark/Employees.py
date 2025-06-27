d0cd13aa37a45e6f780a209c33774ccac62fca9b
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-06-27 11:47:25 +0530

    Employees.py

---
+++ PySpark/Employees.py
@@ -0,0 +1,64 @@
+Easy (10 Questions)
+Select all records from employees_df.
+
+Show only employee names and their salaries.
+
+Find all employees who work in department 102.
+
+Count how many employees are in each department.
+
+Show the unique department IDs from employees_df.
+
+List all employee names in ascending order of salary.
+
+Display all records from projects_df where project name contains "Project G".
+
+Count the total number of employees.
+
+Find the minimum and maximum salary.
+
+Rename column salary to emp_salary.
+
+
+Intermediate (10 Questions)
+Join employees_df with departments_df to show employee name and department name.
+
+Find the total salary paid per department.
+
+Show the number of projects assigned to each employee.
+
+Find employees who are not assigned to any project.
+
+Find employees working in the "Engineering" department.
+
+Get average salary per department.
+
+List all employees who are assigned to more than 1 project.
+
+Find the top 3 highest paid employees.
+
+Join all three DataFrames and show employee name, project name, and department name.
+
+Add a new column to employees_df showing if salary > 60,000 (Yes/No).
+
+
+Hard (10 Questions)
+List departments with no employees.
+
+List employees with no department assigned (i.e., unmatched dept_id).
+
+Find the department with the highest average salary.
+
+Find the second highest salary in employees_df.
+
+Create a DataFrame showing each department’s total salary and number of employees.
+
+From joined tables, find employees working on more than one project and earning more than 60,000.
+
+Show all projects and the number of employees assigned to each project.
+
+Perform a self-join on employees_df to find pairs of employees in the same department.
+
+Create a new DataFrame that shows for each department: total salary, avg salary, and max salary.
+
+Identify and list orphan projects (i.e., projects assigned to non-existent employees).
