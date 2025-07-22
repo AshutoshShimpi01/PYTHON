0e5e8f6b5ca2500dbdd09efd2b7cd8669dbc49fb
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-07-22 19:37:56 +0530

    PySpark-Pandas.py

---
+++ Data-Engineer-syllabus/PySpark-Pandas.py
@@ -0,0 +1,11 @@
+| Feature               | **Pandas**                                             | **PySpark**                                             |
+| --------------------- | ------------------------------------------------------ | ------------------------------------------------------- |
+| **Purpose**           | Used for **small to medium-sized** datasets            | Designed for **large-scale** distributed data           |
+| **Speed**             | Runs on a **single machine (in-memory)**               | Runs on **multiple machines** using Spark engine        |
+| **Memory Usage**      | May crash if data doesn’t fit in RAM                   | Handles huge data sets by **distributing** workload     |
+| **API Style**         | Simple and intuitive **Python API**                    | Similar API, but uses **lazy evaluation**               |
+| **Performance**       | Faster on **small data**                               | Faster on **big data** (>1GB or distributed data)       |
+| **DataFrame Backend** | Built on **NumPy**                                     | Built on **Resilient Distributed Datasets (RDD)**       |
+| **Best Use Case**     | Ideal for **exploration or prototyping**               | Ideal for **big data processing and ETL pipelines**     |
+| **Execution Model**   | **Eager execution** — runs immediately                 | **Lazy execution** — waits until an action is triggered |
+| **Integration**       | Works well with **Python tools** (matplotlib, sklearn) | Integrates with **Hadoop, Hive, BigQuery**, etc.        |
