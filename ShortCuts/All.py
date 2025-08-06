c5df9af1285bd29a88fd1cc7c6231ebf031bf364
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-08-06 13:20:43 +0530

    Update All.py

--- ShortCuts/All.py
+++ ShortCuts/All.py
@@ -7,5 +7,9 @@ Python
 https://github.com/AshutoshShimpi01/PYTHON/blob/main/Data-Engineer-syllabus/Related-Python.py
 https://github.com/AshutoshShimpi01/PYTHON/blob/main/DE-Python-Inter-Qs/1st.py
 
+Best for Python logics code-
+https://github.com/AshutoshShimpi01/PYTHON/blob/main/DE-Python-Inter-Qs/Build-Thinking-Codes.py
+
+
 
 
