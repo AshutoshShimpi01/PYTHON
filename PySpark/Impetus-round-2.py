b953cb875b640b66e67a48cfa88cab1cc618f136
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-10-21 09:43:55 +0530

    Update Impetus-round-2.py

--- PySpark/Impetus-round-2.py
+++ PySpark/Impetus-round-2.py
@@ -38,3 +38,47 @@ GROUP BY
     city
 HAVING
     COUNT(*) > 3; -- Filtering the aggregated groups (cities);
+
+
+
+   
+Question 3 : Find the second highest transaction for first city 
+
+WITH RankedTransactions AS (
+    -- Step 1: Rank ALL transactions, partitioned by city
+    SELECT
+        *,
+        DENSE_RANK() OVER (PARTITION BY city ORDER BY t_amount DESC) AS rank_num
+    FROM
+        transactions
+)
+-- Step 2: Select the row where the rank is 2 AND the city matches the 'first' city
+SELECT
+    city,
+    t_id,
+    c_id,
+    t_amount
+FROM
+    RankedTransactions
+WHERE
+    -- Filter 1: Isolate the second highest transaction
+    rank_num = 2
+    AND
+    -- Filter 2: Isolate the transaction belonging to the 'first' city (alphabetically)
+    city = (SELECT MIN(city) FROM transactions);
+
+
+------
+SUBQUERY
+------
+
+SELECT
+    t.transaction_amount
+FROM
+    transactions t
+WHERE
+    t.city = (SELECT MIN(city) FROM transactions) -- Isolate the transactions of the first city
+ORDER BY
+    t.transaction_amount DESC
+OFFSET 1 -- Skip the 1st highest transaction
+LIMIT 1;  -- Take the next one (the 2nd highest)
